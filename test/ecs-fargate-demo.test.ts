d6a82b68454aed017fc063a7d74bad83f792ba22
George Chen <qchea@amazon.com>
2023-01-07 12:03:47 -0600

    Initial commit

---
+++ test/ecs-fargate-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsFargateDemo from '../lib/ecs-fargate-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-fargate-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsFargateDemo.EcsFargateDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
