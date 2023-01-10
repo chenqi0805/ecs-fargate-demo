ac87c6edf36a94505b85451fbafabd511c8a9f84
George Chen <qchea@amazon.com>
2023-01-09 20:44:58 -0600

    MAINT: change sample app container

--- bin/ecs-fargate-demo.ts
+++ bin/ecs-fargate-demo.ts
@@ -8,7 +8,7 @@ import {
 } from "../lib/ecs-fargate-alb-logging-service-discovery-demo-stack";
 
 const app = new cdk.App();
-new EcsFargateAlbLoggingServiceDiscoveryDemoStack(app, 'EcsFargateDemoStack', {
+new EcsFargateAlbDemoStack(app, 'EcsFargateDemoStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/ecs-fargate-alb-demo-stack.ts
+++ lib/ecs-fargate-alb-demo-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import {Vpc} from "aws-cdk-lib/aws-ec2";
-import {Cluster, ContainerImage} from "aws-cdk-lib/aws-ecs";
+import {Cluster, ContainerImage, FargateTaskDefinition, Protocol} from "aws-cdk-lib/aws-ecs";
 import {ApplicationLoadBalancedFargateService} from "aws-cdk-lib/aws-ecs-patterns";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -18,12 +18,32 @@ export class EcsFargateAlbDemoStack extends cdk.Stack {
     const vpc = new Vpc(this, 'MyVpc', { maxAzs: 2 });
     const cluster = new Cluster(this, 'Cluster', { vpc });
 
+    const taskDef = new FargateTaskDefinition(this, "MyTaskDefinition", {
+      memoryLimitMiB: 512,
+      cpu: 256,
+    })
+
+    taskDef.addContainer("AppContainer", {
+      image: ContainerImage.fromRegistry("httpd:2.4"),
+      portMappings: [{
+        containerPort: 80,
+        hostPort: 80,
+        protocol: Protocol.TCP
+      }],
+      entryPoint: ["sh", "-c"],
+      command:  [
+        "/bin/sh -c \"echo '<html> <head> <title>Amazon ECS Sample App</title> " +
+        "<style>body {margin-top: 40px; background-color: #333;} </style> </head><body> " +
+        "<div style=color:white;text-align:center> <h1>Amazon ECS Sample App</h1> <h2>Congratulations!</h2> <p>Your " +
+        "application is now running on a container in Amazon ECS.</p> </div></body></html>' > " +
+        "/usr/local/apache2/htdocs/index.html && httpd-foreground\""
+      ],
+    })
+
     // Instantiate Fargate Service with just cluster and image
     new ApplicationLoadBalancedFargateService(this, "FargateService", {
       cluster,
-      taskImageOptions: {
-        image: ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-      },
+      taskDefinition: taskDef,
       desiredCount: 3
     });
   }
